8b84d17af701c7ee712e9bf95ca456ff9ddd2efa
kritika <kritikasri27@gmail.com>
2020-09-06 16:02:39 +0530

    Added main logic of generating Quotes by calling Quotes API

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, {Component} from "react"
+import "./index.css"
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading:false,
+            quotes: {}
+        }
+    }
+           
+    tweetQuote(){
+        const twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quotes.content} - ${this.state.quotes.author}`;
+        window.open(twitterUrl, '_blank');
+    }
+
+    componentDidMount() {
+        this.getQuote()
+     }
+  
+     getQuote() {
+        this.setState({loading: true})
+        fetch("https://api.quotable.io/random")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    quotes: data
+                    
+                })
+                
+            })
+    }
+
+
+    getNewQuote = () => { 
+          this.getQuote()
+     }
+  
+    render() {
+   return (              
+    
+    <div className="box-centerside">
+        <div id="text">
+            <p style={{textAlign:"center"}}>"{this.state.quotes.content}"</p></div>
+        <div id="author"><h5 style={{textAlign:"center"}}>{this.state.quotes.author}</h5>
+        <div className="button-container">
+        <button className="twitter-button"  onClick={()=>this.tweetQuote()}>
+                <i className="fab fa-twitter"></i>
+            </button>
+           
+            <button onClick={this.getNewQuote}>
+                New Quote</button>
+            </div>
+        </div>
+    </div> 
+    
+        )} 
+        
+        }      
+        
+
+
+export default App
