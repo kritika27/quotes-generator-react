d68e7e9d587df520d1f071db990d1eed1afbab4e
kritika <kritikasri27@gmail.com>
2020-09-27 20:07:05 +0530

    Modified footer & style

--- src/App.js
+++ src/App.js
@@ -1,65 +1,59 @@
-import React, {Component} from "react"
-import "./index.css"
+import React, { Component } from "react";
+import "./index.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            loading:false,
-            quotes: {}
-        }
-    }
-           
-    tweetQuote(){
-        const twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quotes.content} - ${this.state.quotes.author}`;
-        window.open(twitterUrl, '_blank');
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: {},
+    };
+  }
 
-    componentDidMount() {
-        this.getQuote()
-     }
-  
-     getQuote() {
-        this.setState({loading: true})
-        fetch("https://api.quotable.io/random")
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
-                    loading: false,
-                    quotes: data
-                    
-                })
-                
-            })
-    }
+  tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quotes.content} - ${this.state.quotes.author}`;
+    window.open(twitterUrl, "_blank");
+  }
 
+  componentDidMount() {
+    this.getQuote();
+  }
 
-    getNewQuote = () => { 
-          this.getQuote()
-     }
-  
-    render() {
-   return (              
-    
-    <div className="box-centerside">
+  getQuote() {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          quotes: data,
+        });
+      });
+  }
+
+  getNewQuote = () => {
+    this.getQuote();
+  };
+
+  render() {
+    return (
+      <div className="box-centerside">
         <div id="text">
-            <p style={{textAlign:"center"}}>"{this.state.quotes.content}"</p></div>
-        <div id="author"><h5 style={{textAlign:"center"}}>{this.state.quotes.author}</h5>
-        <div className="button-container">
-        <button className="twitter-button"  onClick={()=>this.tweetQuote()}>
-                <i className="fab fa-twitter"></i>
-            </button>
-           
-            <button onClick={this.getNewQuote}>
-                New Quote</button>
-            </div>
+          <p style={{ textAlign: "center" }}>"{this.state.quotes.content}"</p>
         </div>
-    </div> 
-    
-        )} 
-        
-        }      
-        
+        <div id="author">
+          <h5 style={{ textAlign: "center" }}>{this.state.quotes.author}</h5>
+          <div className="button-container">
+            <button
+              className="twitter-button"
+              onClick={() => this.tweetQuote()}
+            >
+              <i className="fab fa-twitter"></i>
+            </button>
 
+            <button onClick={this.getNewQuote}>New Quote</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-export default App
+export default App;
